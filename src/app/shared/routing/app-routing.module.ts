e8511cbc36d8c59741b0e4e40dd906e5e0b3918a
Wagner Barbosa <wsoubar@gmail.com>
2019-05-18 17:22:06 -0300

    inicial

---
+++ src/app/shared/routing/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// Required components for which route services to be activated
+import { SignInComponent } from 'src/app/components/sign-in/sign-in.component';
+import { SignUpComponent } from 'src/app/components/sign-up/sign-up.component';
+import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from 'src/app/components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from 'src/app/components/verify-email/verify-email.component';
+// Import canActivate guard services
+import { AuthGuard } from "src/app/shared/guard/auth.guard";
+import { SecureInnerPagesGuard } from "src/app/shared/guard/secure-inner-pages.guard.ts.guard";
+// Include route guard in routes array
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'register-user', component: SignUpComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'verify-email-address', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard] }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
