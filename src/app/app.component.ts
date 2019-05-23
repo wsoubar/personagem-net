66d0241e9f790e569ee1b8960cd2efd752323e41
Wagner Barbosa <wsoubar@gmail.com>
2019-05-23 05:40:17 -0300

    testes com a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'personagens';
+  //isLogged : boolean = false;
+
+  constructor(private authService: AuthService) {
+  }
+
 }
